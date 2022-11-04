44f39b44adfa47cbda08bc1fca4ecea3f686b715
DanielRivasMD <danielrivasmd@gmail.com>
2022-11-04 11:10:08 +0100

    add just config

---
+++ .just.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#################################################################################
+
+# remote address
+ulamID="drivas@ulam.icm.uu.se"
+localHOME="/Users/drivas"
+remoteHOME="/home/drivas"
+projPath="Factorem/fun_motifs_plugin"
+projRemote="${remoteHOME}/${projPath}"
+projLocal="${localHOME}/${projPath}"
+
+#################################################################################
