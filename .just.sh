a2253e0c132b38df34f02c0ab2901fa32b2fd552
DanielRivasMD <danielrivasmd@gmail.com>
2023-02-02 13:32:50 +0100

    update just config

--- .just.sh
+++ .just.sh
@@ -4,6 +4,7 @@
 
 # remote address
 ulamID="drivas@ulam.icm.uu.se"
+uppmaxID="drivas@rackham.uppmax.uu.se"
 localHOME="/Users/drivas"
 remoteHOME="/home/drivas"
 projPath="Factorem/fun_motifs_plugin"
