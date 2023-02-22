0927ddecba0f3456f2889e9e5b2f6f4b56b40981
DanielRivasMD <danielrivasmd@gmail.com>
2023-02-22 15:23:40 +0100

    update overlay function

--- src/process/overlay.rs
+++ src/process/overlay.rs
@@ -9,38 +9,58 @@ use config::{
   File,
   FileFormat,
 };
+use std::path::Path;
 use std::path::PathBuf;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
+use crate::utils::reader::byte_file_reader;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn overlay(motif: String, output_dir: String, chromatin_dir: String, chromatin_files: Vec<String>) -> anyResult<()> {
+pub fn overlay(motif_sites_input_file: &str, motifs_overlapping_tracks_output_dir: &str, chromatin_tracks_dir_path: &str, chromatin_tracks_files: Vec<String>) -> anyResult<String> {
+
+  let motifs_overlapping_tracks_file = String::new();
 
   // read motif file
+  let mut f = byte_file_reader(&motif_sites_input_file)?;
+
+  // check motif file is in chromatin dir
 
   // iterate on chromatin files
-  for f in chromatin_files {
-
-    // check motif file is in chromatin dir
+  for chr_n_file in chromatin_tracks_files {
 
     // define overlap file
+    let motifs_overlapping_tracks_file = format!("{}/{}_overlapping_tracks.bed7", motifs_overlapping_tracks_output_dir, motif_sites_input_file);
+    let motifs_overlapping_tracks_file_tmp = format!("{}_tmp", motifs_overlapping_tracks_file);
 
     // create files
+    if !Path::new(&motifs_overlapping_tracks_file).exists() {
+
+      let motif_sites_input_file_sorted = format!("{}_sorted", motif_sites_input_file);
+      let chromatin_tracks_input_file = format!("{}/{}", chromatin_tracks_dir_path, chr_n_file);
+      let chromatin_tracks_input_file_sorted = format!("{}_sorted", chromatin_tracks_input_file);
+
+      println!("Intersecting: {} & {}", motif_sites_input_file, chromatin_tracks_input_file)
+      // intersecting: /proj/snic2020-16-187/nobackup/funMotifs_analysis/funMotifs_datafiles/Motifs_26_03/motifs_per_chr//chrX.bed and /proj/snic2020-16-187/nobackup/funMotifs_analysis/Weight_training_logit_12_05/results/processed_data/chromatin_marks_all_cells_onlynarrowpeaks/chrX.bed
 
     // sort files by columns
+    // TODO: sort like `sort -k1,1 -k2,2 -k3,3`
 
     // do some bedtool operation
+    // TODO: BedTool stuff
 
     // reformat temporary file
+    // TODO: reformat function
 
     // based reformat on column 6 (zero-indexed)
 
+    }
+
   }
 
-  Ok(())
+  Ok(motifs_overlapping_tracks_file)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
